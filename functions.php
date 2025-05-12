c900a0ced19be5b27cc7558ec546c38db522e1a4
Peter Morty <76602521+PeterMorty@users.noreply.github.com>
2025-05-12 13:12:07 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,3 +34,76 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+/** Add logo to admin page */
+function admin_login_logo() {
+	echo '
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+		width: 100%;
+		height: 60px;
+  		background-image: url( ' . wp_upload_dir()['url'] . '/logo.svg );
+		background-size: contain
+        }
+    </style>';
+}
+function admin_login_url($url) {
+    return site_url();
+}
+add_action( 'login_enqueue_scripts', 'admin_login_logo' );
+add_filter( 'login_headerurl', 'admin_login_url' );
+
+/** Disable wordpress comments */
+// Disable support for comments and trackbacks in post types
+function disable_comments_post_types_support() {
+    $post_types = get_post_types();
+ 
+    foreach ($post_types as $post_type) {
+        if(post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+}
+add_action('admin_init', 'disable_comments_post_types_support');
+ 
+// Close comments on the front-end
+function disable_comments_status() {
+    return false;
+}
+add_filter('comments_open', 'disable_comments_status', 20, 2);
+add_filter('pings_open', 'disable_comments_status', 20, 2);
+ 
+// Hide existing comments
+function disable_comments_hide_existing_comments($comments) {
+    $comments = array();
+    return $comments;
+}
+add_filter('comments_array', 'disable_comments_hide_existing_comments', 10, 2);
+ 
+// Remove comments page in menu
+function disable_comments_admin_menu() {
+    remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'disable_comments_admin_menu');
+ 
+// Redirect any user trying to access comments page
+function disable_comments_admin_menu_redirect() {
+    global $pagenow;
+    if ($pagenow === 'edit-comments.php') {
+        wp_redirect(admin_url()); exit;
+    }
+}
+add_action('admin_init', 'disable_comments_admin_menu_redirect');
+ 
+// Remove comments metabox from dashboard
+function disable_comments_dashboard() {
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'disable_comments_dashboard');
+
+/** Shortcode: Languages menu */
+function shortcode_languages_menu() {
+	echo wp_nav_menu( array('menu' => 'Idiomas') );
+}
+add_shortcode('languages_menu', 'shortcode_languages_menu');
